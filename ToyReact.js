8aacb42935c7c34f982f4beb42251cb21f4be438
guo <1172432373@qq.com>
2020-08-06 19:45:34 +0800

    第三天

--- ToyReact.js
+++ ToyReact.js
@@ -1,47 +1,100 @@
+let childrenSymbol = Symbol("children");
 class ElementWrapper{
     constructor(type){
-        this.root = document.createElement(type)  // 创建的实Dom
+        // this.root = document.createElement(type)  // 创建的实Dom
+        this.type = type            
+        this.props = Object.create(null)
+        this[childrenSymbol] = []
+        this.children =[]
     }
     setAttribute(name,value){
-        if( name.match(/^on([\s\S]+)$/) ){  // 处理事件
-            // console.log(RegExp.$1)  [\s\S]表示所有字符
-            let event = RegExp.$1.replace(/^[\s\S]/,s => s.toLowerCase());
-            this.root.addEventListener(event,value)
-        }
-        if (name === 'className') { name = 'class' };
-        this.root.setAttribute(name,value)
+        // if( name.match(/^on([\s\S]+)$/) ){  // 处理事件
+        //     // console.log(RegExp.$1)  [\s\S]表示所有字符
+        //     let event = RegExp.$1.replace(/^[\s\S]/,s => s.toLowerCase());
+        //     this.root.addEventListener(event,value)
+        // }
+        // if (name === 'className') { name = 'class' };
+        // this.root.setAttribute(name,value)
+        this.props[name] = value; 
     }
+
     appendChild(vchild){
-        let range = document.createRange();
-        if(this.root.children.length){
-            range.setStartAfter(this.root.lastChild);
-            range.setEndAfter(this.root.lastChild)
-        }else{
-            range.setStart(this.root,0)
-            range.setEnd(this.root,0)
-        }
-        vchild.mountTo(range)
+        // let range = document.createRange();
+        // if(this.root.children.length){
+        //     range.setStartAfter(this.root.lastChild);
+        //     range.setEndAfter(this.root.lastChild)
+        // }else{
+        //     range.setStart(this.root,0)
+        //     range.setEnd(this.root,0)
+        // }
+        // vchild.mountTo(range)
+        this[childrenSymbol].push(vchild)
+        this.children.push(vchild.vdom)
     }
-    mountTo(range){
+    get vdom(){
+        return this;
+    }
+    mountTo(range) {   // 虚拟dom所有的操作都放到mountTo 产生实dom来做
+        this.range = range
+        
+        let placeholder = document.createComment("placeholder")
+        let endRange = document.createRange();
+        endRange.setStart(range.endContainer,range.endOffset)
+        endRange.setEnd(range.endContainer, range.endOffset)
+        endRange.insertNode(placeholder)
+
         range.deleteContents();
-        range.insertNode(this.root)
-        // parent.appendChild(this.root)
+        let element = document.createElement(this.type);
+        for (let name  in this.props) {   //setAttribute
+            let value = this.props[name];
+            // 处理事件
+            if( name.match(/^on([\s\S]+)$/) ){   //[\s\S]表示所有字符
+                let event = RegExp.$1.replace(/^[\s\S]/,s => s.toLowerCase());
+                element.addEventListener(event,value)
+            }
+            if (name === 'className') { name = 'class' };
+
+            element.setAttribute(name,value)
+        };
+        for (let child of this.children) { // appendChild
+            let range = document.createRange();
+            if(element.children.length){
+                range.setStartAfter(element.lastChild);
+                range.setEndAfter(element.lastChild)
+            }else{
+                range.setStart(element,0)
+                range.setEnd(element,0)
+            }
+            child.mountTo(range)
+        };
+        // range.deleteContents();
+        range.insertNode(element)
     }
 }
 class TextWrapper{   
     constructor(content){
         this.root = document.createTextNode(content)
+        this.type = "#text"
+        this.props = Object.create(null)
+        this.children = []
     }
     mountTo(range) {
+        this.range = range;
         range.deleteContents();
         range.insertNode(this.root)
         // parent.appendChild(this.root)
+    }
+    get vdom(){
+        return this;
     }
 }
 export class Component{    // 公共方法
     constructor(){
         this.children = []
         this.props = Object.create(null);
+    }
+    get type(){
+        return this.constructor.name;
     }
     setAttribute(name, value) {
         this.props[name] = value;
@@ -55,15 +108,78 @@ export class Component{    // 公共方法
 
     }
     update(){
-        let placeholder = document.createComment("placeholder")
-        let range = document.createRange()
-        range.setStart(this.range.endContainer,this.range.endOffset)
-        range.setEnd(this.range.endContainer, this.range.endOffset)
-        range.insertNode(placeholder)
-        this.range.deleteContents();
+        // let placeholder = document.createComment("placeholder")
+        // let range = document.createRange()
+        // range.setStart(this.range.endContainer,this.range.endOffset)
+        // range.setEnd(this.range.endContainer, this.range.endOffset)
+        // range.insertNode(placeholder)
+        // this.range.deleteContents();
 
-        let vdom = this.render()
-        vdom.mountTo(this.range)
+        let vdom = this.vdom
+        if (this.oldVdom){
+            let isSameNode= (node1,node2) => {
+                if(node1.type!== node2.type) 
+                    return false;
+                for (let name in node1.props) { 
+                    // if (typeof node1.props[name] === "function" && typeof node2.props[name] === "function"  // 对事件的处理
+                    //     && node1.props[name].toString() === node2.props[name].toString())
+                    //     continue;
+                    if (typeof node1.props[name] === "object" && typeof node2.props[name] === "object"  // 对事件的处理
+                        && JSON.stringify(node1.props[name]) === JSON.stringify(node2.props[name]))
+                        continue;
+
+                    if (node1.props[name] !== node2.props[name])
+                        return false;
+                }
+                if (Object.keys(node1.props).length !== Object.keys(node2.props).length ){
+                    return false;
+                }
+                    
+                return true;
+            }
+            let isSameTree = (node1,node2) =>{
+                if (!isSameNode(node1,node2)){
+                    return false;
+                }
+                if (node1.children.length !== node2.children.length ){
+                    return false;
+                }
+                for (let i = 0; i < node1.children.length; i++) {
+                    if (!isSameTree(node1.children[i], node2.children[i])){
+                        return false
+                    } 
+                }
+                return true;
+            }
+
+            let replace = (newTree,oldTree) =>{
+                if (isSameTree(newTree, oldTree))   // 新旧dom比对
+                    return;
+
+                if (!isSameNode(newTree, oldTree)) { // 新旧根节点对比
+                    newTree.mountTo(oldTree.range)
+
+                } else {
+                    for (let i = 0; i < newTree.children.length; i++) {
+                        replace( newTree.children[i], oldTree.children[i] )
+                        
+                    }
+                }
+            }
+            replace(vdom, this.oldVdom)
+            // console.log( vdom)
+            // console.log(this.vdom)
+
+        }else{  
+            // 第一次mountTO
+            vdom.mountTo(this.range)
+        }
+
+        this.oldVdom = vdom;
+    }
+
+    get vdom(){
+        return this.render().vdom;
     }
     appendChild(vchild){
         this.children.push(vchild)
